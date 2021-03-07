f6469014308c93cb9edc222fdcbc9cb6104e7b9e
TomasTomcany <xtomcany@stuba.sk>
2021-03-07 15:03:12 +0100

    converted

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 
 
 // globalna premenna pre funkciu memory_init
@@ -16,14 +17,14 @@ void memory_init(void* ptr, unsigned int size) {
     // v hlavicke zaciatku nastavime velkost celeho volneho bloku, musime odpocitat od zadanej velkosti velkost hlavicky
     // a konca pre buducnost na rozlisenie ci je blok alokovany alebo nie rozlisime znamienkami pri velkosti bloku v
     // hlavicke, alokovany blok ma +, nealokovany -
-    *((int *)ptr) = -(size - 3*sizeof(int));
-    *((int *)(ptr+size-2*sizeof(int))) = -(size - 3*sizeof(int));
+    *((int *)ptr) = -((int)size - 3*sizeof(int));
+    *((int *)(ptr+size-2*sizeof(int))) = -((int)size - 3*sizeof(int));
     // nastavenie konca
     *((int *)(ptr+size-sizeof(int))) = 1;
 }
 
 void* memory_alloc(unsigned int size) {
-    size += (4 - size % 4);             // padding, na rychlejsi presun medzi hlavickami
+    size += (4 - size % 4);             // padding, na rychlejsi presun medzi hlavickami a prevencia fragmentacie
 
     // metodou first fit najdeme volne miesto pre blok
     int *current = start;                      // pointer prehladavanych hlaviciek nastavime na zaciatok
@@ -50,9 +51,9 @@ void* memory_alloc(unsigned int size) {
         size += original_size;
     }
 
-    (*current) = size;                                  // hlavicka
-    int *footer = (current+(size+sizeof(int))/4);       // posun na paticku
-    *footer = size;                                     // paticka
+    (*current) = (int)size;                                  // hlavicka
+    int *footer = (current+(size+sizeof(int))/4);            // posun na paticku
+    *footer = (int)size;                                     // paticka
 
     // velkost noveho prazdneho bloku je original velkost - velkost alokovaneho bloku - velkost hlavicky a paticky
     int free_size = -(original_size - size - 2*sizeof(int));
@@ -62,10 +63,14 @@ void* memory_alloc(unsigned int size) {
     free_block = (free_block-(free_size-sizeof(int))/4);
     (*free_block) = free_size;                                                  // paticka
 
-    return current+1;
+    return ((void *)current+sizeof(int));                               // pointer ma ukazovat na prve slovo v bloku
 }
 
 int memory_free(void* valid_ptr) {
+    if (valid_ptr == NULL){
+        return 1;
+    }
+
     valid_ptr -= sizeof(int);                          // presun na hlavicku bloku
     int size = *((int *)valid_ptr);
     // zmena znamienka na nastavenie alokovaneho bloku na volny
@@ -74,7 +79,7 @@ int memory_free(void* valid_ptr) {
     // coalescing - spajanie volnych blokov nachadzajucich sa vedla seba na znizenie fragmentacie pamate
     // najpr sa pozrieme na nasledujuci blok a pripadne spojime
     int *next = ((int *)valid_ptr + (size + 2*sizeof(int))/4);
-    if (*next <= -1){           // ak je blok nealokovany
+    if (*next <= -1){            // ak je blok nealokovany
         // zmenim velkost v hlavicke uvolneneho bloku na povodna + nasledujuca + velkost hlavicky a paticky,
         // ktorych pamat bude uvolnena na alokovanie
         int new_size = -size + *next - 2*sizeof(int);
@@ -84,9 +89,9 @@ int memory_free(void* valid_ptr) {
         int *new_footer = ((int *)valid_ptr-(new_size-sizeof(int))/4);
         *new_footer = new_size;             //nastavenie novej paticky
     }
-    else{                                 // ak je blok alokovany
+    else{                       // ak je blok alokovany
         int *new_footer = ((int *)valid_ptr+(size+sizeof(int))/4);
-        *new_footer = -size;
+        *new_footer = -size;    // zmena znamienka paticky
     }
 
     // ak zadany pointer je zaciatok haldy, neexistuje predosly blok a mozme funkciu skoncit
@@ -122,37 +127,85 @@ int memory_check(void* ptr) {
             return 1;
         }
 
-        if (size > 1) {                               // ak je blok alokovany
-            current += (size + 2 * sizeof(int))/4;    // posun na dalsiu hlavicku
-        }
-        else{                                         // ak je blok nealokovany
-            current -= (size - 2 * sizeof(int))/4;    // posun na dalsiu hlavicku
-        }
-
+        current += (abs(size) + 2 * sizeof(int))/4;    // posun na dalsiu hlavicku
         size = *((int *) current);
     }
     return 0;
 }
-
-int main(void) {
-    char region[101];
-    memory_init(&region, 101);
+void debug_tester(){
+    char region[150];
+    memory_init(region, 101);
     char *ptr1 = (char *) memory_alloc(10);
-    int a1 = memory_check(ptr1+8);
+    int a1 = memory_check(ptr1);
     char *ptr2 = (char *) memory_alloc(30);
-    int a2 = memory_check(ptr2-16);
+    int a2 = memory_check(ptr2);
     char *ptr3 = (char *) memory_alloc(10);
-    int a3 = memory_check(ptr3+2);
-    char *ptr4 = (char *) memory_alloc(40);
+    int a3 = memory_check(ptr3);
+    char *ptr4 = (char *) memory_alloc(40);          // alokacia bloku ktory sa nezmesti
     int a4 = memory_check(ptr4);
-    memory_free(ptr1);
+    memory_free(ptr1);                                    // uvolnenie bloku na zaciatku, funkcia sa nepozera mimo pamat
     int a5 = memory_check(ptr1);
     char *ptr5 = (char *) memory_alloc(8);
-    memory_free(ptr3);
+    memory_free(ptr3);                                   // spajanie s nasledujucim blokom
     int a6 = memory_check(ptr3);
-    memory_free(ptr2);
+    memory_free(ptr2);                                   // spajanie s predchadzajucim blokom
     int a7 = memory_check(ptr2);
     int a = 40;
-    int a8 = memory_check(&a);
+    int a8 = memory_check(&a);                           // memory check na adresu mimo pamate
+    int a9 = memory_check(ptr2+2);                   // memory check v vnutri bloku
+}
+
+void tester(char *region, int min_memory, int max_memory, int min_block, int max_block, int random_blocks){
+    int ideal = 0;                  // idealny pocet alokovanych bytov
+    int ideal_blocks = 0;           // idealny pocet alokovanych blokov
+    int real_blocks = 0;            // realny pocet alokovanych blokov
+    char *ptr[100000];              // pole pointrov na ulozenie pre memory_alloc na uvolnenie pomocou memory_free
+    int i = 0;                      // index pola pointrov
+
+
+    int memory = (rand() % (max_memory-min_memory+1)) + min_memory;    // nahodna velkost pamate vyhradena v parametroch
+    memory_init(region, memory);
+
+    int block = (rand() % (max_block-min_block+1)) + min_block;
+    while(ideal <= memory - min_block){         // dokym v idealnom rieseni nebude dostatok volnej pamate na alokovanie
+        if (random_blocks == 1){                                        // ked maju byt bloky nerovnake
+            block = (rand() % (max_block-min_block+1)) + min_block;
+            if (ideal + block > memory){continue;}          // posun na dalsiu iteraciu pokial sa momentalny blok nezmesti
+        }
+        ideal += block;
+        ideal_blocks++;
+        ptr[i] = memory_alloc(block);
+        if (ptr[i]){                    // ak sa blok uspesne alokoval mozeme ho pripocitat do celkoveho poctu
+            real_blocks++;
+            i++;
+        }
+    }
+
+    // uvolnenie pamate
+    for (int j = 0; j <= i; j++){
+        memory_free(ptr[j]);
+        j++;
+    }
+
+    // vyhodnotenie % uspesnosti alokovania oproti realnemu rieseniu
+    float block_result = ((float)real_blocks/ideal_blocks)*100;
+    printf("Na pamati o velkosti %d bytov bolo alokovanych "
+           "%.2f%% blokov v porovnani s idealnym riesenim.\n", memory, block_result);
+}
+
+int main() {
+    // po spusteni by nemal davat ziadny error, sledovanie pamate v debugging tool ci vsetko funguje tak ako ma
+    debug_tester();
+
+    char region[1000000];
+    srand(time(0));
+    for (int i = 0; i < 10;i++) {
+        //tester(region, 50, 200, 8, 24, 0);
+        //tester(region, 50, 200, 8, 24, 1);
+        //tester(region, 5000, 500000, 500, 5000, 1);
+        tester(region, 50000, 1000000, 8, 50000, 1);
+    }
     return 0;
 }
+
+
